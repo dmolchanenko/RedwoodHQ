34e1f6d3da233af5175d2f4e223f4f1ccd61eac7
Dmitri Molchanenko <dmitrim@primatest.net>
2013-06-04 22:12:32 -0700

    auto comment

--- public/automationscripts/Sample/src/actions/selenium/Browser.groovy
+++ public/automationscripts/Sample/src/actions/selenium/Browser.groovy
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.ie.InternetExplorerDriver
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeDriverService
 
 class Browser{
   
