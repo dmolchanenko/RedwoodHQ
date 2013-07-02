dc8d600f3d22900e9e0091c9b7f24186ec229729
Dmitri Molchanenko <dmitrim@primatest.net>
2013-07-01 22:10:54 -0700

    fixed issue #94

--- public/ux/codeeditor/search.js
+++ public/ux/codeeditor/search.js
@@ -102,14 +102,14 @@
         var replaceTextField = Ext.getCmp("replaceTextField");
         var text = replaceTextField.getValue();
         if (all) {
-            cm.compoundChange(function() { cm.operation(function() {
+            cm.operation(function() {
                 for (var cursor = getSearchCursor(cm, query); cursor.findNext();) {
                     if (typeof query != "string") {
                         var match = cm.getRange(cursor.from(), cursor.to()).match(query);
                         cursor.replace(text.replace(/\$(\d)/, function(w, i) {return match[i];}));
                     } else cursor.replace(text);
                 }
-            })});
+            });
         } else {
             //clearSearch(cm);
             var cursor = getSearchCursor(cm, query,cm.getCursor(true) );
