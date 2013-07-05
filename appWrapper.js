bb59155d78c0317d74384238af4535340e67b8ba
Dmitri Molchanenko <dmitrim@primatest.net>
2013-07-04 21:14:28 -0700

    now db port number is taken from config file

--- appWrapper.js
+++ appWrapper.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var path = require('path');
 var dbPath =  __dirname + "/vendor/MongoDB/bin/mongod.exe";
 var logPath = __dirname + "/logs";
+var common = require('./common');
 
 if (process.argv[2] === "--stop"){
     if (fs.existsSync(__dirname+"/app.pid")){
@@ -23,43 +24,48 @@ if (process.argv[2] === "--stop"){
     return;
 }
 
-if (!fs.existsSync(logPath)){
-    fs.mkdirSync(logPath);
-}
+common.parseConfig(function(){
 
-var appChild = new (forever.Monitor)('app.js', {
-    silent: false,
-    options: [],
-    killTree: true,
-    'outFile': logPath+'/app.out.log',
-    'errFile': logPath+'/app.err.log'
-});
-
-var dbChild = forever.start([ dbPath ], {
-    max : 1,
-    silent : false,
-    'outFile': logPath+'/db.out.log',
-    'errFile': logPath+'/db.err.log'
-});
-
-var dbStarted = false;
-var dbOut = "";
-
-dbChild.on('stdout', function (data) {
-    if (dbStarted == false){
-        dbOut = dbOut + data.toString();
-        if (dbOut.indexOf("waiting for connections on port") != -1){
-            dbStarted = true;
-            appChild.start();
-        }
-        //console.log('stdout: ' + data);
-        //console.log('stdout: ' + data);
+    if (!fs.existsSync(logPath)){
+        fs.mkdirSync(logPath);
     }
+
+    var appChild = new (forever.Monitor)('app.js', {
+        silent: false,
+        options: [],
+        killTree: true,
+        'outFile': logPath+'/app.out.log',
+        'errFile': logPath+'/app.err.log'
+    });
+
+    var dbChild = forever.start([ dbPath,"--port",common.Config.DBPort ], {
+        max : 1,
+        silent : false,
+        'outFile': logPath+'/db.out.log',
+        'errFile': logPath+'/db.err.log'
+    });
+
+    var dbStarted = false;
+    var dbOut = "";
+
+    dbChild.on('stdout', function (data) {
+        if (dbStarted == false){
+            dbOut = dbOut + data.toString();
+            if (dbOut.indexOf("waiting for connections on port") != -1){
+                dbStarted = true;
+                appChild.start();
+            }
+            //console.log('stdout: ' + data);
+            //console.log('stdout: ' + data);
+        }
+    });
+
+    dbChild.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
+
 });
 
-dbChild.stderr.on('data', function (data) {
-    console.log('stderr: ' + data);
-});
 
 
 
