2572f6a0ae585be22271d2d76bedd38499d834e9
Dmitri Molchanenko <dmitrim@primatest.net>
2014-02-11 11:53:56 -0800

    fixed issue #330

--- agent/common.js
+++ agent/common.js
@@ -91,10 +91,10 @@ exports.initLogger = function(fileName){
     this.logger = new (winston.Logger)({
         transports: [
             new (winston.transports.Console)(),
-            new (winston.transports.File)({ filename: 'logs/'+fileName+'.log',maxsize:10485760,maxFiles:10,timestamp:true })
+            new (winston.transports.File)({ filename: '../logs/'+fileName+'.log',maxsize:10485760,maxFiles:10,timestamp:true })
         ]
     });
-    this.logger.handleExceptions(new winston.transports.File({ filename: 'logs/'+fileName+'_errors.log' }));
+    this.logger.handleExceptions(new winston.transports.File({ filename: '../logs/'+fileName+'_errors.log' }));
     this.logger.exitOnError = false;
 };
 
