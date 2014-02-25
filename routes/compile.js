71344f5ee0e53cb8d0ae304cd29e8438fabfb3d7
Dmitri Molchanenko <dmitrim@primatest.net>
2014-02-25 14:06:18 -0800

    changed from spawn to exec for the compile command

--- routes/compile.js
+++ routes/compile.js
@@ -1,4 +1,4 @@
-var spawn = require('child_process').spawn;
+var spawn = require('child_process').exec;
 var compileProcs = {};
 var path = require('path');
 var common = require('../common');
@@ -7,13 +7,11 @@ exports.operation = function(msg, id,callback,onFinish){
     if (compileProcs[id] != undefined){
         compileProcs[id].proc.kill();
     }
-    //var buildDir = __dirname.replace("\\routes","")+"/public/automationscripts/"+msg.project;
-    //var antDir = __dirname.replace("\\routes","")+"/vendor/ant/bin/";
     var buildDir = path.resolve(__dirname,"../public/automationscripts/"+msg.project)+"/"+msg.username;
     var antDir = path.resolve(__dirname,"../vendor/ant/bin/")+"/";
     var javaDir = path.resolve(__dirname,"../vendor/Java");
-    compileProcs[id] = {proc:spawn(antDir+'ant.bat',['clean','compile','jar'],{cwd: buildDir,timeout:1800000,env:{JAVA_HOME:javaDir}}),status:"compile"};
-    //compileProcs[id] = {proc:spawn(antDir+'ant.bat',['clean','compile','jar'],{cwd: buildDir,timeout:1800000,env:{JAVA_HOME:javaDir,_JAVACMD:javaDir+"\\bin\\java.exe -version:1.7+"}}),status:"compile"};
+    compileProcs[id] = {proc:spawn(antDir+'ant.bat clean compile jar',{cwd: buildDir,timeout:1800000,env:{JAVA_HOME:javaDir}}),status:"compile"};
+    //compileProcs[id] = {proc:spawn(antDir+'ant.bat',['clean','compile','jar'],{cwd: buildDir,timeout:1800000,env:{JAVA_HOME:javaDir}}),status:"compile"};
     common.logger.info(antDir);
     common.logger.info(buildDir);
     compileProcs[id].proc.stdout.on('data', function(data) {
