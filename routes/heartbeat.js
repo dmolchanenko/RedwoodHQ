9b5f7fc309c525cc085cd5f25f85188c06209832
Dmitri Molchanenko <dmitrim@primatest.net>
2014-07-25 09:17:39 -0700

    now will update only 5 agents at a time, put an extra check for machine state

--- routes/heartbeat.js
+++ routes/heartbeat.js
@@ -31,7 +31,7 @@ exports.heartbeatPost = function(req, res){
                 var localVersion = parseInt(app.Config.AgentVersion.replace(/\./g,''));
                 var agentVersion = parseInt(data.agentVersion.replace(/\./g,''));
                 if((agentVersion < localVersion) && (data.OS == "Windows")){
-                    if(!updatingAgents[ip]){
+                    if(machine.state !== "Updating" && !updatingAgents[ip] && Object.keys(updatingAgents).length < 6){
                         updateMachine(app.getDB(),machine._id,{$set:{macAddress:data.macAddress,vncport:data.vncport,port:data.port,state:"Updating"}});
                         updatingAgents[ip] = ip;
                         sendFileToAgent(path.resolve(__dirname,"../public/agentsetup/")+"/Agent_RedwoodHQ_Setup.exe","Agent_RedwoodHQ_Setup.exe",ip,data.port,4,function(){
