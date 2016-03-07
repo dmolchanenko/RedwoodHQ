72f17c1803a7a6f5b0d297bf535a0e34b0492a59
s asfd <asdf@asfd.com>
2016-03-07 13:59:55 -0800

    now agent will retry if update did not work

--- agent/routes/update.js
+++ agent/routes/update.js
@@ -1,9 +1,33 @@
 var spawn = require('child_process').spawn;
+var common = require('../common');
 
 exports.Post = function(req, res){
     res.contentType('json');
     res.json({
         success: true
     });
-    spawn('Agent_RedwoodHQ_Setup.exe', ['/S'],{detached:true});
+    updateAgent(5);
 };
+
+function updateAgent(retryCount){
+    var updateProc = spawn('Agent_RedwoodHQ_Setup.exe', ['/S'],{detached:true});
+    updateProc.stdout.on('data', function (data) {
+        //cliData = cliData + data.toString();
+    });
+
+    updateProc.stderr.on('data', function (data) {
+    });
+
+    updateProc.on('close', function (code) {
+        if(code != 0){
+            //retry
+            if(retryCount <= 0){
+                common.logger.error('unable to update agent error: ');
+            }
+            else{
+                retryCount--;
+                setTimeout(updateAgent(retryCount),30000)
+            }
+        }
+    });
+}
