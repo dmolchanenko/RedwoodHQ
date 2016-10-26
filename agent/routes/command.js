e600d651e29999505798a51846571191f46dae0c
s asfd <asdf@asfd.com>
2016-10-26 14:34:53 -0700

    look for python in 2 places

--- agent/routes/command.js
+++ agent/routes/command.js
@@ -221,7 +221,12 @@ function startLauncher(executionID,threadID,type,callback){
             else{
                 //pythonLibPath = pathDivider+baseExecutionDir+"/"+executionID+"/lib/python2.7/site-packages";
                 pythonLibPath = pathDivider+baseExecutionDir+"/"+executionID+"/lib/site-packages";
-                pythonPath = "/usr/bin/python"
+                if(fs.existsSync("/usr/bin/python")){
+                    pythonPath = "/usr/bin/python"
+                }
+                else{
+                    pythonPath = "/usr/local/bin/python"
+                }
             }
             launcherProc[executionID+portNumber.toString()] = spawn(pythonPath,[pythonLauncherPath,portNumber.toString()],{env:{PATH:process.env.PATH+pathDivider+baseExecutionDir+"/"+executionID+"/bin/",PYTHONPATH:path.resolve(__dirname,"../../vendor/Python/DLLs")+pathDivider+path.resolve(__dirname,"../../vendor/Python/lib")+pathDivider+baseExecutionDir+"/"+executionID+"/src/"+pythonLibPath},cwd:baseExecutionDir+"/"+executionID+"/bin/"});
         }
